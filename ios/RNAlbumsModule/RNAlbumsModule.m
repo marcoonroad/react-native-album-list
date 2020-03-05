a6b4ce0bedbebd07c135467474cc2727f8585252
Marco Aurélio da Silva <marcoonroad@gmail.com>
2020-03-05 12:47:15 -0300

    wip: :construction: migration
    Signed-off-by: Marco Aurélio da Silva <marco.aurelio@totalcommit.com>
    Signed-off-by: Marco Aurélio da Silva <marcoonroad@gmail.com>

--- ios/RNAlbumsModule/RNAlbumsModule.m
+++ ios/RNAlbumsModule/RNAlbumsModule.m
@@ -32,20 +32,24 @@ RCT_EXPORT_METHOD(getAlbumList:(NSDictionary *)options
                                                options:nil];
       __block NSMutableArray<NSDictionary *> *result = [[NSMutableArray alloc] init];
       [collections enumerateObjectsUsingBlock:^(PHAssetCollection * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-        PHAssetCollectionSubtype type = [obj assetCollectionSubtype];
-        if (!isAlbumTypeSupported(type)) {
-          return;
-        }
-        
+        //PHAssetCollectionSubtype type = [obj assetCollectionSubtype];
+        //if (!isAlbumTypeSupported(type)) {
+        //  return;
+        //}
+
         PHFetchOptions *fetchOptions = [[PHFetchOptions alloc] init];
         fetchOptions.predicate = [NSPredicate predicateWithFormat:@"mediaType == %d", PHAssetMediaTypeImage];
         fetchOptions.sortDescriptors = @[ [NSSortDescriptor sortDescriptorWithKey:@"creationDate" ascending:YES] ];
         PHFetchResult *fetchResult = [PHAsset fetchAssetsInAssetCollection:obj options: fetchOptions];
         PHAsset *coverAsset = fetchResult.lastObject;
-          
+        NSString *burstIdentifier = @"";
+        if (coverAsset.burstIdentifier) {
+            burstIdentifier = coverAsset.burstIdentifier;
+        }
+
         if (coverAsset) {
             NSDictionary *album = @{@"count": @(fetchResult.count),
-                                    @"name": albumNameFromType(type),
+                                    @"name": burstIdentifier,
                                     // Photos Framework asset scheme ph://
                                     // https://github.com/facebook/react-native/blob/master/Libraries/CameraRoll/RCTPhotoLibraryImageLoader.m
                                     @"cover": [NSString stringWithFormat:@"ph://%@", coverAsset.localIdentifier] };
@@ -66,19 +70,16 @@ typedef void (^authorizeCompletion)(BOOL);
 + (void)authorize:(authorizeCompletion)completion {
   switch ([PHPhotoLibrary authorizationStatus]) {
     case PHAuthorizationStatusAuthorized: {
-      // 已授权
       completion(YES);
       break;
     }
     case PHAuthorizationStatusNotDetermined: {
-      // 没有申请过权限，开始申请权限
       [PHPhotoLibrary requestAuthorization:^(PHAuthorizationStatus status) {
         [RNAlbumsModule authorize:completion];
       }];
       break;
     }
     default: {
-      // Restricted or Denied, 没有授权
       completion(NO);
       break;
     }
@@ -89,37 +90,36 @@ typedef void (^authorizeCompletion)(BOOL);
 
 #pragma mark - 
 
-static NSString *albumNameFromType(PHAssetCollectionSubtype type) {
-  switch (type) {
-    case PHAssetCollectionSubtypeSmartAlbumUserLibrary: return @"UserLibrary";
-    case PHAssetCollectionSubtypeSmartAlbumSelfPortraits: return @"SelfPortraits";
-    case PHAssetCollectionSubtypeSmartAlbumRecentlyAdded: return @"RecentlyAdded";
-    case PHAssetCollectionSubtypeSmartAlbumTimelapses: return @"Timelapses";
-    case PHAssetCollectionSubtypeSmartAlbumPanoramas: return @"Panoramas";
-    case PHAssetCollectionSubtypeSmartAlbumFavorites: return @"Favorites";
-    case PHAssetCollectionSubtypeSmartAlbumScreenshots: return @"Screenshots";
-    case PHAssetCollectionSubtypeSmartAlbumBursts: return @"Bursts";
-    case PHAssetCollectionSubtypeSmartAlbumVideos: return @"Videos";
-    case PHAssetCollectionSubtypeSmartAlbumSlomoVideos: return @"SlomoVideos";
-    case PHAssetCollectionSubtypeSmartAlbumDepthEffect: return @"DepthEffect";
-    default: return @"null";
-  }
-}
+//static NSString *albumNameFromType(PHAssetCollectionSubtype type) {
+//  switch (type) {
+//    case PHAssetCollectionSubtypeSmartAlbumUserLibrary: return @"UserLibrary";
+//    case PHAssetCollectionSubtypeSmartAlbumSelfPortraits: return @"SelfPortraits";
+//    case PHAssetCollectionSubtypeSmartAlbumRecentlyAdded: return @"RecentlyAdded";
+//    case PHAssetCollectionSubtypeSmartAlbumTimelapses: return @"Timelapses";
+//    case PHAssetCollectionSubtypeSmartAlbumPanoramas: return @"Panoramas";
+//    case PHAssetCollectionSubtypeSmartAlbumFavorites: return @"Favorites";
+//    case PHAssetCollectionSubtypeSmartAlbumScreenshots: return @"Screenshots";
+//    case PHAssetCollectionSubtypeSmartAlbumBursts: return @"Bursts";
+//    case PHAssetCollectionSubtypeSmartAlbumVideos: return @"Videos";
+//    case PHAssetCollectionSubtypeSmartAlbumSlomoVideos: return @"SlomoVideos";
+//    case PHAssetCollectionSubtypeSmartAlbumDepthEffect: return @"DepthEffect";
+//    default: return @"null";
+//  }
+//}
 
-static BOOL isAlbumTypeSupported(PHAssetCollectionSubtype type) {
-  switch (type) {
-    case PHAssetCollectionSubtypeSmartAlbumUserLibrary:
-    case PHAssetCollectionSubtypeSmartAlbumSelfPortraits:
-    case PHAssetCollectionSubtypeSmartAlbumRecentlyAdded:
-    case PHAssetCollectionSubtypeSmartAlbumTimelapses:
-    case PHAssetCollectionSubtypeSmartAlbumPanoramas:
-    case PHAssetCollectionSubtypeSmartAlbumFavorites:
-    case PHAssetCollectionSubtypeSmartAlbumScreenshots:
-    case PHAssetCollectionSubtypeSmartAlbumBursts:
-    case PHAssetCollectionSubtypeSmartAlbumDepthEffect:
-      return YES;
-    default:
-      return NO;
-  }
-}
-
+//static BOOL isAlbumTypeSupported(PHAssetCollectionSubtype type) {
+//  switch (type) {
+//    case PHAssetCollectionSubtypeSmartAlbumUserLibrary:
+//    case PHAssetCollectionSubtypeSmartAlbumSelfPortraits:
+//    case PHAssetCollectionSubtypeSmartAlbumRecentlyAdded:
+//    case PHAssetCollectionSubtypeSmartAlbumTimelapses:
+//    case PHAssetCollectionSubtypeSmartAlbumPanoramas:
+//    case PHAssetCollectionSubtypeSmartAlbumFavorites:
+//    case PHAssetCollectionSubtypeSmartAlbumScreenshots:
+//    case PHAssetCollectionSubtypeSmartAlbumBursts:
+//    case PHAssetCollectionSubtypeSmartAlbumDepthEffect:
+//      return YES;
+//    default:
+//      return NO;
+//  }
+//}
